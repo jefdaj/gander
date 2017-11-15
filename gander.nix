46dd25eb5cac1ac50c49c528dd81c33dccc8e0fe
jefdaj <1198065+jefdaj@users.noreply.github.com>
2017-11-15 02:20:51 -0800

    start writing scan

--- gander.nix
+++ gander.nix
@@ -1,11 +1,11 @@
-{ mkDerivation, base, docopt, directory-tree, stdenv }:
+{ mkDerivation, base, docopt, filepath, directory-tree, stdenv }:
 mkDerivation {
   pname = "gander";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base docopt directory-tree ];
+  executableHaskellDepends = [ base docopt filepath directory-tree ];
   description = "The \"Git ANnex DEdupeR\"";
   license = stdenv.lib.licenses.lgpl3;
 }
