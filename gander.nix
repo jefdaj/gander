ce641663d4dbf079c79c4e4309e182df054d5c6d
jefdaj <1198065+jefdaj@users.noreply.github.com>
2017-11-16 10:10:06 -0800

    got scan (hashTree) to work!

--- gander.nix
+++ gander.nix
@@ -1,11 +1,18 @@
-{ mkDerivation, base, docopt, filepath, directory-tree, stdenv }:
+{ mkDerivation, base, docopt, directory-tree, stdenv
+, cryptohash, bytestring }:
 mkDerivation {
   pname = "gander";
   version = "0.1.0.0";
   src = ./.;
   isLibrary = false;
   isExecutable = true;
-  executableHaskellDepends = [ base docopt filepath directory-tree ];
+  executableHaskellDepends = [
+    base
+    docopt
+    directory-tree
+    cryptohash
+    bytestring
+  ];
   description = "The \"Git ANnex DEdupeR\"";
   license = stdenv.lib.licenses.lgpl3;
 }
