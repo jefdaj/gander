6f56c05e11311acfa1581e333e29613969266347
Jeffrey David Johnson <jefdaj@protonmail.ch>
2017-11-16 18:35:40 -0800

    Squashed commit of the following:
    commit 4fbdb50789f3a3536c7a97bfcc6fa7d47b3430ba
    Author: root <root@fedora-23-dvm>
    Date:   Thu Nov 16 18:07:03 2017 -0800
    
        got deserialize working on larger dirs
    
    commit 8b0e929ea7fd5b0c9deefdc04f8ed403c48c4fdb
    Author: root <root@fedora-23-dvm>
    Date:   Thu Nov 16 17:51:30 2017 -0800
    
        wip on deserialize: simple test works
    
    commit 2bc8c95284831fb4988dc12a683a357f6140ce7e
    Author: root <root@fedora-23-dvm>
    Date:   Thu Nov 16 14:08:31 2017 -0800
    
        wip on deserialize

--- gander.hs
+++ gander.hs
@@ -4,6 +4,7 @@ module Main where
 
 -- TODO break into modules? Main, Types, Config, Scan, Dedup
 -- TODO is git-annex an actual dep, or just recommended to go with it?
+-- TODO figure out how to read files + compute hashes in parallel
 
 import qualified Data.ByteString.Lazy  as LB
 import qualified System.Directory.Tree as DT
@@ -11,12 +12,12 @@ import qualified System.Directory.Tree as DT
 import Crypto.Hash                (Digest, SHA256, hashlazy)
 import Data.ByteString.Lazy.Char8 (pack)
 import Data.Either                (partitionEithers)
-import Data.List                  (sort)
+import Data.List                  (sort, partition)
 import System.Console.Docopt      (docoptFile, parseArgsOrExit,
                                    getArgOrExitWith, isPresent, longOption,
                                    shortOption, command, argument)
 import System.Environment         (getArgs)
-import System.FilePath            ((</>))
+import System.FilePath            ((</>), takeFileName, takeDirectory, splitPath)
 
 -----------
 -- types --
@@ -37,8 +38,8 @@ data Options = Options
  - TODO would storing it in a more efficient way help?
  - TODO would adding timestamps or number of files help?
  -}
-newtype Hash = Hash (Digest SHA256)
-  deriving (Eq, Show)
+newtype Hash = Hash String
+  deriving (Eq, Read, Show)
 
 {- A tree of file names matching (a subdirectory of) the annex,
  - where each dir and file node contains a hash of its contents.
@@ -50,33 +51,57 @@ newtype Hash = Hash (Digest SHA256)
 data HashTree
   = File { name :: FilePath, hash :: Hash }
   | Dir  { name :: FilePath, hash :: Hash, contents :: [HashTree] }
-  deriving (Show) -- TODO write Eq instance
+  deriving (Eq, Read, Show) -- TODO write Eq instance
 
 {- A map from file/dir hash to a list of duplicate file paths.
  - Could be rewritten to contain links to HashTrees if that helps.
  -}
 data PathsByHash = Map Hash [FilePath]
-  deriving Show
+  deriving (Read, Show)
 
----------------------------
--- read/write hash trees --
----------------------------
+-------------------------------------
+-- serialize and deserialize trees --
+-------------------------------------
 
 serialize :: HashTree -> String
 serialize = unlines . serialize' ""
 
 serialize' :: FilePath -> HashTree -> [String]
-serialize' dir (File n (Hash h)   ) = [unwords [show h, dir </> n]]
+serialize' dir (File n (Hash h)   ) = [unwords [h, "file", dir </> n]]
 serialize' dir (Dir  n (Hash h) cs)
-  = map (dir </>) (concatMap (serialize' $ dir </> n) cs) -- recurse on contents
-  ++ [unwords [show h, n]] -- finish with hash of entire dir
+  = concatMap (serialize' $ dir </> n) cs -- recurse on contents
+  ++ [unwords [h, "dir ", dir </> n]] -- finish with hash of entire dir
 
-----------
--- scan --
-----------
+-- TODO error on null string/lines?
+-- TODO wtf why is reverse needed? remove that to save RAM
+deserialize :: String -> HashTree
+deserialize = snd . head . foldr accTrees [] . map readLine . reverse . lines
 
-hashBytes :: LB.ByteString -> Digest SHA256
-hashBytes = hashlazy
+{- This one is confusing! It accumulates a list of trees and their indent
+ - levels, and when it comes across a dir it uses the indents to determine
+ - which files are children to put inside it vs which are siblings.
+ -}
+accTrees :: (Hash, String, Int, FilePath) -> [(Int, HashTree)] -> [(Int, HashTree)]
+accTrees l@(h, t, indent, p) cs = case t of
+  "file" -> cs ++ [(indent, File p h)]
+  "dir"  -> let (children, siblings) = partition (\(i, t) -> i > indent) cs
+                dir = Dir p h $ map snd children
+            in siblings ++ [(indent, dir)]
+  _ -> error $ "invalid line: '" ++ show l ++ "'" 
+
+readLine :: String -> (Hash, String, Int, FilePath)
+readLine line = (Hash h, t, i, takeFileName p)
+  where
+    [h, t]   = words tmp            -- first two words are hash and type
+    (tmp, p) = splitAt 70 line      -- rest of the line is the path
+    i        = length $ splitPath p -- get indent level from path
+
+----------------------
+-- build hash trees --
+----------------------
+
+hashBytes :: LB.ByteString -> String
+hashBytes = show . (hashlazy :: LB.ByteString -> Digest SHA256)
 
 -- see https://stackoverflow.com/a/30537010
 -- TODO this should match git-annex! maybe use its code directly?
@@ -89,7 +114,7 @@ hashFile path = do
 hashHashes :: [Hash] -> Hash
 hashHashes hs = Hash $ hashBytes $ pack txt
   where
-    txt = unlines $ sort $ map (\(Hash h) -> show h) hs
+    txt = unlines $ sort $ map (\(Hash h) -> h) hs
 
 hashTree :: DT.DirTree Hash -> Either String HashTree
 hashTree (DT.Failed n e ) = Left  $ n ++ " " ++ show e
@@ -99,10 +124,31 @@ hashTree (DT.Dir    n ts) = case partitionEithers (map hashTree ts) of
   (errors, _) -> Left  $ unlines errors
 
 -- TODO rename hashDir?
-scan :: Options -> FilePath -> IO (DT.DirTree Hash)
+-- Note that you can't hash a folder while writing to a file inside it!
+scan :: Options -> FilePath -> IO HashTree
 scan opts path = do
-  (_ DT.:/ tree) <- DT.readDirectoryWithL hashFile path
-  return tree
+  (_ DT.:/ tree) <- DT.readDirectoryWith hashFile path
+  let tree' = hashTree tree
+  case tree' of
+    Left  e -> error e
+    Right t -> return t
+
+-----------
+-- tests --
+-----------
+
+roundTrip :: FilePath -> IO Bool
+roundTrip path = do
+  let opts = Options False False False
+  tree1 <- scan opts path
+  let str1  = serialize   tree1
+      tree2 = deserialize str1
+      str2  = serialize   tree2
+  putStrLn str1
+  putStrLn $ show tree1
+  putStrLn $ "tree1 == tree2? " ++ show (tree1 == tree2)
+  putStrLn $ "str1 == str2? "   ++ show (str1  == str2)
+  return $ tree1 == tree2
 
 ----------
 -- main --
@@ -124,5 +170,5 @@ main = do
         }
   -- dispatch on command
   if cmd "scan"
-    then path "path" >>= scan opts >>= putStrLn . show
+    then path "path" >>= scan opts >>= putStrLn . serialize
     else print args >> print opts
