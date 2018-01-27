3200bb25c7618f7db9a31072e8fc2dfa6b0209b2
jefdaj <1198065+jefdaj@users.noreply.github.com>
2018-01-26 17:13:06 -0800

    disperse cmd fns into their modules

--- Gander/Cmd/Dupes.hs
+++ Gander/Cmd/Dupes.hs
@@ -1,1 +1,12 @@
 module Gander.Cmd.Dupes where
+
+import Gander.Config
+import Gander.Lib.HashTree (deserializeTree)
+import Gander.Lib.DupeMap
+
+cmdDupes :: Config -> FilePath -> IO ()
+cmdDupes _ path = do
+  tree <- fmap deserializeTree $ readFile path
+  let pbyh = pathsByHash tree
+      pdup = dupesByNFiles pbyh
+  printDupes pdup
--- Gander/Cmd/Hash.hs
+++ Gander/Cmd/Hash.hs
@@ -1,1 +1,14 @@
-module Gander.Cmd.Hash where
+module Gander.Cmd.Hash
+  ( cmdHash
+  )
+  where
+
+import Gander.Config
+import Gander.Lib.HashTree (excludeGlobs, printHashes)
+import System.Directory.Tree (readDirectoryWithL)
+
+-- Note that you can't hash a folder while writing to a file inside it!
+cmdHash :: Config -> FilePath -> IO ()
+cmdHash cfg path = do
+  tree <- readDirectoryWithL return path
+  printHashes (verbose cfg) $ excludeGlobs (exclude cfg) tree
