72dfc2ce3bed27348fd356b0e487048a85ff3ece
Jeffrey David Johnson <jefdaj@protonmail.ch>
2018-01-27 01:35:05 -0800

    got diff command working

--- Gander/Cmd.hs
+++ Gander/Cmd.hs
@@ -1,10 +1,12 @@
 module Gander.Cmd
   ( cmdHash
+  , cmdDiff
   , cmdDupes
   , cmdTest
   )
   where
 
 import Gander.Cmd.Hash
+import Gander.Cmd.Diff
 import Gander.Cmd.Dupes
 import Gander.Cmd.Test
