8adc2f7a829bd55db9c93358e3a4954f3bb1b4b0
Jeffrey David Johnson <jefdaj@protonmail.ch>
2018-01-27 16:35:37 -0800

    todos

--- Gander/Cmd/Add.hs
+++ Gander/Cmd/Add.hs
@@ -1,1 +1,7 @@
 module Gander.Cmd.Add where
+
+-- TODO rsync files over
+--   haskell fn or regular system call?
+--   add to nix dependencies
+-- TODO diff of before and after hashtrees
+--   error if not null
--- Gander/Cmd/Dedup.hs
+++ Gander/Cmd/Dedup.hs
@@ -1,1 +1,14 @@
 module Gander.Cmd.Dedup where
+
+-- TODO explain to user:
+--   list (first N) duplicates
+--   what would you like to do?
+--     skip for now
+--     skip always (ignore)
+--     save one of them by number
+--     save to a new path you enter
+--     quit
+-- TODO "collapse" fn:
+--   rm all but the one picked, double-checking they all still have the same hash
+--   (error if not)
+--   move the one picked if not at final destination
