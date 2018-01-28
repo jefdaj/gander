4086b11d0a6bf624cadc3912721ff9da9aae3088
jefdaj <1198065+jefdaj@users.noreply.github.com>
2018-01-27 16:35:37 -0800

    todos

--- Gander/Cmd/Update.hs
+++ Gander/Cmd/Update.hs
@@ -1,1 +1,6 @@
 moduel Gander.Cmd.Update where
+
+-- TODO read old and new hash files (not dirs for now)
+-- TODO determine path of new one inside old (error if not)
+-- TODO replace old subdir with whole new tree
+-- TODO write... to stdout? outfile?
