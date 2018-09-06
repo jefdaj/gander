9aa270cecb4d066b1b0099d24f277fffe9657da7
jefdaj <1198065+jefdaj@users.noreply.github.com>
2018-09-06 18:18:10 +0000

    todos

--- Gander.hs
+++ Gander.hs
@@ -3,7 +3,6 @@
 module Main where
 
 -- TODO sort by how many links deduping would save: files per dupe * (dupes - 1)
--- TODO break into modules? Main, Types, Config, Scan, Dedup
 -- TODO is git-annex an actual dep, or just recommended to go with it?
 -- TODO figure out how to read files + compute hashes in parallel
 
