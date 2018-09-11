ca1aaa269eb6de8c75fd8c4cdfb1cdeb2940997d
jefdaj <1198065+jefdaj@users.noreply.github.com>
2018-09-11 01:47:12 +0000

    add annex dupes command, refactor init/hash

--- Gander/Cmd/AnnexHash.hs
+++ Gander/Cmd/AnnexHash.hs
@@ -1,9 +1,13 @@
 module Gander.Cmd.AnnexHash where
 
+import Gander.Lib       (runGit)
+import Data.Maybe       (fromJust)
 import Control.Monad    (when)
 import Gander.Config    (Config(..))
 import Gander.Lib       (buildTree, readTree, serializeTree, diff, printDiffs)
 import System.Directory (doesFileExist)
+
+-- TODO have buildTree accept a list of paths; should be doing unsorted + sorted at least
 
 -- annex command prints messages and writes to hashes.txt
 cmdAnnexHash :: Config -> FilePath -> FilePath -> IO ()
@@ -17,6 +21,10 @@ cmdAnnexHash cfg hashes target = do
   else do
     when (verbose cfg) $ putStrLn "creating hashes.txt"
   writeFile hashes $ serializeTree new
+  let dir = fromJust $ annex cfg
+  out1 <- runGit dir ["add", "hashes.txt"]
+  out2 <- runGit dir ["commit", "-m", "gander hash"]
+  when (verbose cfg) $ putStr $ concat [out1, out2]
   -- TODO git add hashes.txt + git commit here
   -- putStrLn "done"
 
--- Gander/Cmd/Init.hs
+++ Gander/Cmd/Init.hs
@@ -19,10 +19,8 @@ cmdInit cfg dir = do
   out2 <- runGit dir ["config", "user.name", "'gander user'"]
   out3 <- runGit dir ["config", "user.email", "f@ke.email"]
   out4 <- runGit dir ["annex", "init", takeFileName dir]
+  when (verbose cfg) $ putStr $ concat [out1, out2, out3, out4]
   cmdAnnexHash cfg (dir </> "hashes.txt") (dir </> "unsorted") -- TODO option to do this later?
-  out5 <- runGit dir ["add", "hashes.txt"]
-  out6 <- runGit dir ["commit", "-m", "gander init"]
-  when (verbose cfg) $ putStr $ concat [out1, out2, out3, out4, out5, out6]
 
 -- check that init has been run, or prompt user to do that first
 guardInit :: Config -> FilePath -> IO ()
