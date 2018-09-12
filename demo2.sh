211262ef2010a2849779cd1bf74ed16bd888204e
jefdaj <1198065+jefdaj@users.noreply.github.com>
2018-09-12 02:29:35 +0000

    add unicode test to demo2.sh

--- demo2.sh
+++ demo2.sh
@@ -1,36 +1,55 @@
 #!/usr/bin/env bash
 
-rm -rf demo
+rm -rf demo demo-hashes.txt
 mkdir demo
-pushd demo
+cd demo
 
-# create 100 dirs with 10 files each
-for d in {1..100}; do
-  mkdir dir${d}
+# The unicode test files are from:
+# https://github.com/bits/UTF-8-Unicode-Test-Documents
+
+echo -n "generating some horribly-named files..."
+(for d in {1..100}; do
+  unicodefile="$(ls ../unicode/* | shuf | head -n1)"
+  baddir="$(cat "$unicodefile" | shuf | head -n1)"
+  mkdir -p "$baddir"
   for f in {1..10}; do
-    echo "this is file number ${f}" >> dir${d}/file${f}.txt
+    unicodefile="$(ls ../unicode/* | shuf | head -n1)"
+    # badname="$(cat "$unicodefile" | shuf | head -n1 | cut -c-50)" # TODO the cut breaks gander??
+    badname="$(cat "$unicodefile" | shuf | head -n1)"
+    dst="${baddir}/${badname}"
+    echo "this is the content of file $f" >> "$dst"
   done
-done
+done) 2>/dev/null
+echo " done"
 
-# put them randomly inside each other
+echo "the test dataset has $(find -type d | wc -l) folders with $(find -type f | wc -l) files"
+echo "are only 10 unique files though"
+
+echo -n "putting the folders randomly inside each other..."
 SRC=(`ls | shuf`)
 DST=(`ls | shuf`)
 for n in {0..50}; do
   d=${DST[$n]}
   [[ -a $d ]] || continue
-  for i in {1..3}; do
+  for i in {1..10}; do
     s=${SRC[$((n + i))]}
     [[ -a $s ]] || continue
-    [[ $s == d ]] && continue
+    [[ $s == $d ]] && continue
     mv $s $d
   done
 done
+echo " done"
 
-# list dupes
-# should group dirs by tree structure (one dir with two others in it etc)
-# and there should be 10 groups of 100 duplicate files each
-popd
-gander dupes demo | less -R
+cd ..
+
+echo -n "hashing them with gander..."
+gander hash demo > demo-hashes.txt
+echo " done"
+echo
+
+echo "you can explore the demo folder and demo-hashes.txt to get an idea if you want"
+echo -n "then press ENTER to look for duplicates"; read dummyvar
+gander dupes demo-hashes.txt | less -R
 
 # TODO annex init, move the dirs into unsorted, and annex add them
 # TODO annex sort it back to just one dir with 10 files if possible
