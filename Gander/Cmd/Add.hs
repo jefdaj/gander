25243a7f1e89d94d3bf4c32bb5aa92aee71cbb7a
Jeffrey David Johnson <jefdaj@protonmail.ch>
2018-09-17 01:48:22 +0000

    clean up old code

--- Gander/Cmd/Add.hs
+++ Gander/Cmd/Add.hs
@@ -1,6 +1,5 @@
 module Gander.Cmd.Add where
 
--- TODO guess and check hashes
 -- TODO add rsync to nix dependencies
 
 import Gander.Lib
@@ -10,13 +9,6 @@ import Gander.Run    (safeRunDeltas, runRsync)
 import Data.Maybe      (fromJust)
 import System.FilePath ((</>))
 
--- TODO easier to split into two completely different cases? add or rsync+add
--- both start by assembling an Add
--- then apply it (consider rsync + apply it here later)
--- then calculate after hashes
--- if check flag, check those against actual hashes
--- write after hashes
-
 cmdAdd :: Config -> FilePath -> Maybe FilePath -> IO ()
 cmdAdd cfg dst mSrc = do
   let aPath = fromJust $ annex cfg
@@ -25,26 +17,8 @@ cmdAdd cfg dst mSrc = do
     Nothing -> buildTree (verbose cfg) (exclude cfg) dst
     Just s  -> rsyncAndHash cfg s dst'
   let ds  = [Add dst' dstTree]
-      msg = unwords ["add", dst]
+      msg = unwords ["add", dst] -- TODO sanitize!
   safeRunDeltas cfg ds msg
-
-  -- annexed <- inAnnex $ trace ("dst': " ++ dst') dst'
-  -- when (not annexed) (error "destination should be in a git-annex repo") -- TODO remove?
-  -- old version, which totally works but has to re-hash:
-  -- msgDst <- case mSrc of
-    -- Nothing -> return dst -- files should have been manually added to the annex
-    -- Just s -> do
-      -- _ <- runRsync cfg s dst' -- TODO control verbosity
-      -- before  <- buildTree (verbose cfg) (exclude cfg) s
-      -- after   <- buildTree (verbose cfg) (exclude cfg) dst'
-      -- let missing = diff before after
-      -- when (not $ null missing) $ do
-        -- putStrLn $ "error! final files differ from '" ++ s ++ "':"
-        -- printDeltas missing
-      -- return s
-  -- runGitAnnexAdd cfg dst'
-  -- new <- buildTree (verbose cfg) (exclude cfg) aPath
-  -- updateAnnexHashes cfg new
 
 rsyncAndHash :: Config -> FilePath -> FilePath -> IO HashTree
 rsyncAndHash cfg s dst' = do
