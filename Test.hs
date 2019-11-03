5576ae42ea2e3452c88083472381e39f95bf38eb
jefdaj <1198065+jefdaj@users.noreply.github.com>
2019-11-03 18:42:08 +0000

    add tasty-rerun

--- Test.hs
+++ Test.hs
@@ -1,9 +1,11 @@
 module Main where
 
 import Test.Tasty
+import Test.Tasty.Runners
+import Test.Tasty.Ingredients.Rerun
 
 main :: IO ()
-main = defaultMain tests
+main = defaultMainWithIngredients [rerunningTests [consoleTestReporter]] tests
 
 tests :: TestTree
 tests = testGroup "Tests" []
