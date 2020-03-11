dedd0a87285cc596724956cc161ce8c888763380
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-03-11 23:54:24 +0000

    bump version to 0.1.3.0

--- gander.nix
+++ gander.nix
@@ -1,12 +1,12 @@
 { mkDerivation, ansi-terminal, attoparsec, base, byteable
 , bytestring, cryptonite, directory, directory-tree, docopt
-, filepath, Glob, hashable, hspec, pretty-simple, process
-, QuickCheck, split, stdenv, streaming, streaming-bytestring, unix
-, unordered-containers
+, filepath, Glob, hashable, hspec, monad-parallel, pretty-simple
+, process, QuickCheck, split, stdenv, streaming
+, streaming-bytestring, unix, unordered-containers
 }:
 mkDerivation {
   pname = "Gander";
-  version = "0.1.2.0";
+  version = "0.1.3.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
@@ -14,14 +14,14 @@ mkDerivation {
   libraryHaskellDepends = [
     ansi-terminal attoparsec base byteable bytestring cryptonite
     directory directory-tree docopt filepath Glob hashable
-    pretty-simple process split streaming streaming-bytestring unix
-    unordered-containers
+    monad-parallel pretty-simple process split streaming
+    streaming-bytestring unix unordered-containers
   ];
   executableHaskellDepends = [
     ansi-terminal attoparsec base byteable bytestring cryptonite
     directory directory-tree docopt filepath Glob hashable
-    pretty-simple process split streaming streaming-bytestring unix
-    unordered-containers
+    monad-parallel pretty-simple process split streaming
+    streaming-bytestring unix unordered-containers
   ];
   testHaskellDepends = [ base hspec QuickCheck ];
   description = "The \"Git ANnex DEdupeR\"";
