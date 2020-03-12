645d9b9ae78cbca66f6957db1c0be79c24dc8e24
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-03-12 19:19:25 +0000

    remove static cabal stuff for now, add strict unsafe io

--- Gander/Data/HashTree.hs
+++ Gander/Data/HashTree.hs
@@ -111,7 +111,7 @@ buildTree beVerbose excludes path = do
 buildTree' :: Bool -> DT.AnchoredDirTree FilePath -> IO HashTree
 buildTree' _ (a DT.:/ (DT.Failed n e )) = error $ (a </> n) ++ ": " ++ show e
 buildTree' v (_ DT.:/ (DT.File n f)) = do
-  h <- unsafeInterleaveIO $ hashFile v f
+  !h <- unsafeInterleaveIO $ hashFile v f
   return File { name = n, hash = h }
 buildTree' v (a DT.:/ (DT.Dir n cs)) = do
   let root = a </> n
