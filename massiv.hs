9782001611cf325c7cb1e321d53e620c72833412
jefdaj <1198065+jefdaj@users.noreply.github.com>
2020-03-17 12:25:51 +0000

    wip: start massiv.hs as a test

--- massiv.hs
+++ massiv.hs
@@ -12,17 +12,26 @@ import qualified Data.HashTable.ST.Cuckoo as C
 import qualified Data.HashSet             as S
 import qualified Data.ByteString.Char8 as B
 import qualified Data.HashMap.Strict   as M
+import Data.Hashable      (Hashable(..))
 
 -- TODO any reason to retain F/D information at this stage?
 -- data TreeType = F | D deriving (Eq, Read, Show)
+
+newtype Hash = Hash { unHash :: B.ByteString }
+  deriving (Eq, Read, Show, Ord)
+
+-- This is unrelated to Gander's hashing. It's required to use Data.HashMap
+instance Hashable Hash
+  where
+    hashWithSalt n h = hashWithSalt n (unHash h)
 
 -- the Int is nfiles
 type DupeSet  = (Int, S.HashSet B.ByteString)
 type DupeList = (Int, [B.ByteString]) -- TODO remove?
 
 -- in the actual program, bytestrings are wrapped with the Hash constuctor
-type DupeMap     = M.HashMap     B.ByteString DupeSet
-type DupeTable s = C.HashTable s B.ByteString DupeSet
+type DupeMap     = M.HashMap     Hash DupeSet
+type DupeTable s = C.HashTable s Hash DupeSet
 
 testSets :: [DupeSet]
 testSets =
@@ -31,10 +40,10 @@ testSets =
   ]
 
 -- this compiles, but the table can't be pulled out of ST right?
-testTable :: ST s (C.HashTable s B.ByteString DupeSet)
+testTable :: ST s (C.HashTable s Hash DupeSet)
 testTable = H.fromList
-  [ ("hash0", testSets !! 0)
-  , ("hash1", testSets !! 1)
+  [ (Hash "hash0", testSets !! 0)
+  , (Hash "hash1", testSets !! 1)
   ]
 
 array1 :: Array D Ix1 DupeSet
