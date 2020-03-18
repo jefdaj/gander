bc0873ab60abb71f6d3063cd9ca2c8f3500c9ed3
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-03-18 02:47:45 +0000

    massiv.hs: add TreeType + informative main

--- massiv.hs
+++ massiv.hs
@@ -1,11 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Main where
 
 -- first attempt to get Massiv arrays working along with HashTables
 
-import Data.Massiv.Array
+import qualified Data.Massiv.Array as A
 import Control.Monad.ST
 
 import qualified Data.HashTable.Class     as H
@@ -15,9 +17,13 @@ import qualified Data.ByteString.Char8 as B
 -- import qualified Data.HashMap.Strict   as M
 import Data.Hashable      (Hashable(..))
 import qualified Data.List as L
+import Control.DeepSeq
 
 -- TODO any reason to retain F/D information at this stage?
--- data TreeType = F | D deriving (Eq, Read, Show)
+data TreeType = D | F deriving (Eq, Read, Show, Ord)
+
+instance NFData TreeType
+  where rnf = const ()
 
 newtype Hash = Hash { unHash :: B.ByteString }
   deriving (Eq, Read, Show, Ord)
@@ -28,24 +34,24 @@ instance Hashable Hash
     hashWithSalt n h = hashWithSalt n (unHash h)
 
 -- the Int is nfiles
-type DupeSet  = (Int, S.HashSet B.ByteString)
-type DupeList = (Int, [B.ByteString]) -- TODO remove?
+type DupeSet  = (Int, TreeType, S.HashSet B.ByteString)
+type DupeList = (Int, TreeType, [B.ByteString]) -- TODO remove?
 
 -- in the actual program, bytestrings are wrapped with the Hash constuctor
 -- type DupeMap     = M.HashMap     Hash DupeSet
 -- TODO does this need a forall s.?
 type DupeTable s = C.HashTable s Hash DupeSet
 
-testHS :: [S.HashSet B.ByteString]
+testHS :: [(TreeType, S.HashSet B.ByteString)]
 testHS =
-  [ S.singleton "file0"
-  , S.fromList ["test1/file1", "test1/file1"]
-  , S.fromList ["test2/file1", "test3/file1", "test4/file1"]
-  , S.fromList ["test2/file1", "test3/file1"]
+  [ (F, S.singleton "file0")
+  , (D, S.fromList ["test1/file1", "test1/file1"])
+  , (D, S.fromList ["test2/file1", "test3/file1", "test4/file1"])
+  , (D, S.fromList ["test2/file1", "test3/file1"])
   ]
 
 testDS :: [DupeSet]
-testDS = Prelude.map (\s -> (negate $ S.size s, s)) testHS
+testDS = Prelude.map (\(t, s) -> (negate $ S.size s, t, s)) testHS
 
 testHT :: forall s. ST s (C.HashTable s Hash DupeSet)
 testHT = H.fromList $ Prelude.map
@@ -57,52 +63,56 @@ testHTL :: [(Hash, DupeSet)]
 testHTL = runST $ H.toList =<< testHT
 
 -- TODO is this reasonable?
-type DupeSetVec   = Array N Ix1 DupeSet
-type HashScoreVec = Array N Ix1 (Int, B.ByteString)
+type DupeSetVec   = A.Array A.N A.Ix1 DupeSet
+type HashScoreVec = A.Array A.N A.Ix1 (Int, TreeType, B.ByteString)
 
 vec1 :: DupeSetVec
-vec1 = makeArray Par (Sz1 $ length testDS) (testDS !!)
+vec1 = A.makeArray A.Par (A.Sz1 $ length testDS) (testDS !!)
 
 -- TODO why does it have to be DL?
-vec2 :: Array DL Ix1 Int
-vec2 = iterateN (Sz1 $ length testDS) succ 0
+vec2 :: A.Array A.DL A.Ix1 Int
+vec2 = A.iterateN (A.Sz1 $ length testDS) succ 0
 
-vec3 :: Array D Ix1 (Int, B.ByteString)
-vec3 = makeArray Par (Sz1 $ length testDS) (tmp !!)
+vec3 :: A.Array A.D A.Ix1 (Int, TreeType, B.ByteString)
+vec3 = A.makeArray A.Par (A.Sz1 $ length testDS) (tmp !!)
   where
-   tmp = Prelude.map (\(Hash h, (n, _)) -> (-n, h)) testHTL
+   tmp = Prelude.map (\(Hash h, (n,t,_)) -> (-n, t, h)) testHTL
 
 vec3s :: HashScoreVec
-vec3s = quicksort $ compute vec3
+vec3s = A.quicksort $ A.compute vec3
 
 -- TODO next try to get something like vec3 *inside* ST without using H.toList
 --      first using keys, but then with just indexes if possible
 
 mkVecFromHT :: forall s. C.HashTable s Hash DupeSet -> ST s [DupeSet]
 mkVecFromHT ht = do
-  sets <- H.foldM (\vs (_, v@(n,_)) -> return $ if n < (-1) then (v:vs) else vs) [] ht
-  let unsorted = Data.Massiv.Array.fromList Par sets :: DupeSetVec
-      sorted   = quicksort $ compute unsorted :: DupeSetVec
-  return $ Data.Massiv.Array.toList sorted
+  sets <- H.foldM (\vs (_, v@(n,_,_)) -> return $ if n < (-1) then (v:vs) else vs) [] ht
+  let unsorted = A.fromList A.Par sets :: DupeSetVec
+      sorted   = A.quicksort $ A.compute unsorted :: DupeSetVec
+  return $ A.toList sorted
 
 dupes :: forall s. C.HashTable s Hash DupeSet -> ST s [DupeSet]
-dupes = H.foldM (\vs (_, v@(n,_)) -> return $ if n < (-1) then (v:vs) else vs) []
+dupes = H.foldM (\vs (_, v@(n,_,_)) -> return $ if n < (-1) then (v:vs) else vs) []
 
 -- TODO why is having this separate from the vector step so important?
 testDS2 :: [DupeSet]
 testDS2 = runST $ dupes =<< testHT
 
 sortedDL :: [DupeSet]
-sortedDL = Data.Massiv.Array.toList sorted
+sortedDL = A.toList sorted
   where
     sets     = runST $ dupes =<< testHT
-    unsorted = Data.Massiv.Array.fromList Par sets :: DupeSetVec
-    sorted   = quicksort $ compute unsorted :: DupeSetVec
+    unsorted = A.fromList A.Par sets :: DupeSetVec
+    sorted   = A.quicksort $ A.compute unsorted :: DupeSetVec
 
 finished :: [DupeList]
 finished = Prelude.map fixElem sortedDL
   where
-    fixElem (n, fs) = (negate n, L.sort $ S.toList fs)
+    fixElem (n, t, fs) = (negate n, t, L.sort $ S.toList fs)
 
 main :: IO ()
-main = undefined
+main = do
+  print testHS
+  print testDS
+  print testDS2
+  print finished
