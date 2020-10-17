ee96c935e56b7c097a028bdd510f8acf06176151
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-10-17 13:13:11 -0700

    draft static build for mac

--- release.nix
+++ release.nix
@@ -6,7 +6,7 @@ in
 }:
 
 let
-  inherit (pkgs.haskell.lib) markUnbroken;
+  inherit (pkgs.haskell.lib) markUnbroken overrideCabal justStaticExecutables;
   haskellPackages = pkgs.haskell.packages.${compiler}.override {
     overrides = hpNew: hpOld: {
       niv       = import sources.niv {};
@@ -14,12 +14,32 @@ let
       massiv    = markUnbroken hpOld.massiv;
       docopt    = markUnbroken (hpNew.callCabal2nix "docopt" sources.docopt {});
       gander    = hpNew.callCabal2nix "Gander" ./. {};
+
+      # on osx 10.13.6, this seems to work:
+      gander-static = justStaticExecutables hpNew.gander;
+
+      # on linux, is the rest of this needed?
+      # gander-static =
+      #   overrideCabal
+      #     (justStaticExecutables hpNew.gander);
+      #     (oldDerivation: {
+      #       configureFlags = [
+      #         "--ghc-option=-optl=-static"
+      #         "--ghc-option=-optl=-pthread"
+      #         "--ghc-option=-optl=-L${pkgs.gmp5.static}/lib"
+      #         "--ghc-option=-optl=-L${pkgs.zlib.static}/lib"
+      #         "--ghc-option=-optl=-L${pkgs.glibc.static}/lib"
+      #       ];
+      #     });
     };
   };
   project = haskellPackages.gander;
+  static  = haskellPackages.gander-static;
 in
 {
+  # TODO what are these for?
   project = project;
+  static  = static;
 
   shell = haskellPackages.shellFor {
     packages = p: with p; [
