bc567ca6cd988e5ea2518bc324e3e5ac05b9d5d0
jefdaj <1198065+jefdaj@users.noreply.github.com>
2021-01-27 13:48:29 -0800

    wip: add empty test modules

---
+++ test/HashTreeTest.hs
@@ -0,0 +1,7 @@
+module HashTreeTest where
+
+--     describe "HashTree" $ do
+--       describe "HashTree" $ do
+--         it "builds a tree from the test annex" $ pendingWith "need annex test harness"
+--         it "can be round-tripped to a file" $ pendingWith "need annex test harness"
+--         it "can handle unicode filenames" pending
