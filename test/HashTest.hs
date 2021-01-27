d3345b82dda435e1998feef952b8b62f6c36b2c5
jefdaj <1198065+jefdaj@users.noreply.github.com>
2021-01-27 13:22:43 -0800

    wip: hash tests

--- test/HashTest.hs
+++ test/HashTest.hs
@@ -12,16 +12,22 @@ import Test.HUnit
 unit_hash_a_bytestring :: Assertion
 unit_hash_a_bytestring = unHash (hashBytes "a bytestring") @=? "YTI3MDBmODFhZWE2ZjBm"
 
-unit_hash_an_image :: Assertion
-unit_hash_an_image = do
-  h <- hashFile False "gander.png"
-  unHash h @=? "NWMwYjNlN2FiZTQ5OWZj"
-
 unit_hash_an_empty_file :: Assertion
 unit_hash_an_empty_file = do
   f <- emptySystemTempFile "empty"
   h <- hashFile False f
   unHash h @=? "ZTNiMGM0NDI5OGZjMWMx"
+
+unit_hash_file_contents :: Assertion
+unit_hash_file_contents = do
+  f <- writeSystemTempFile "filename should not matter" "file contents should be hashed"
+  h <- hashFile False f
+  unHash h @=? "MTVjMzcwNmJjODQzYTg0"
+
+unit_hash_an_image :: Assertion
+unit_hash_an_image = do
+  h <- hashFile False "gander.png"
+  unHash h @=? "NWMwYjNlN2FiZTQ5OWZj"
 
 -- TODO unit_hash_a_dir
 -- TODO unit_hash_a_empty_dir
