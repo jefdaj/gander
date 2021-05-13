d57b431a22a3665251c06fad8178db0e221bd2cb
jefdaj <1198065+jefdaj@users.noreply.github.com>
2021-05-13 08:43:32 -0700

    hashforest: buildForest

--- lib/Data/Gander/HashForest.hs
+++ lib/Data/Gander/HashForest.hs
@@ -3,6 +3,7 @@
 module Data.Gander.HashForest
   ( HashForest(..)
   , readForest
+  , buildForest
   )
   where
 
@@ -12,6 +13,7 @@ import Data.Gander.HashTree
 
 import TH.Derive
 import Data.Store             (Store(..))
+import System.FilePath.Glob (matchWith, Pattern, MatchOptions(..))
 
 {- A forest is just a list of trees without an overall content hash. It's used
  - at the top level when reading potentially more than one tree from the
@@ -27,3 +29,6 @@ $($(derive [d|
 
 readForest :: Maybe Int -> [FilePath] -> IO HashForest
 readForest md paths = HashForest <$> mapM (readTree md) paths
+
+buildForest :: Bool -> [Pattern] -> [FilePath] -> IO HashForest
+buildForest beVerbose excludes paths = HashForest <$> mapM (buildTree beVerbose excludes) paths
