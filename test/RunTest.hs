61a64747a035f8072cce824f8284d336ac31abea
jefdaj <1198065+jefdaj@users.noreply.github.com>
2021-05-19 14:41:08 -0700

    merge DepsTest into RunTest

--- test/RunTest.hs
+++ test/RunTest.hs
@@ -1,5 +1,23 @@
 module RunTest where
 
+import Test.Hspec
+import System.Process
+import System.Exit
+
+testDep :: FilePath -> [String] -> Spec
+testDep name args = it (unwords $ name:args) $ do
+  (code, _, _) <- readProcessWithExitCode name args ""
+  code `shouldBe` ExitSuccess
+
+spec_make_sure_git_is_installed :: Spec
+spec_make_sure_git_is_installed = testDep "git" ["--help"]
+
+spec_make_sure_rsync_is_installed :: Spec
+spec_make_sure_rsync_is_installed = testDep "rsync" ["-h"]
+
+spec_make_sure_git_annex_is_installed :: Spec
+spec_make_sure_git_annex_is_installed = testDep "git-annex" ["-h"]
+
 --   describe "Gander.Run" $ do
 --     describe "runRsync"       $ it "behaves properly" pending
 --     describe "runGit"         $ it "behaves properly" pending
