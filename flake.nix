00299df746292e395c69242b726b7c4407698bca
jefdaj <1198065+jefdaj@users.noreply.github.com>
2022-07-15 16:56:44 -0700

    draft flake.nix

---
+++ flake.nix
@@ -0,0 +1,47 @@
+# based on https://discourse.nixos.org/t/another-simple-flake-for-haskell-development/18164
+
+{
+  inputs = {
+    nixpkgs.url = "https://github.com/NixOS/nixpkgs/archive/refs/tags/22.05.tar.gz";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      with nixpkgs.legacyPackages.${system};
+      let
+        t = lib.trivial;
+        hl = haskell.lib;
+
+        name = "gander";
+
+        project = devTools: # [1]
+          let addBuildTools = (t.flip hl.addBuildTools) devTools;
+          in haskellPackages.developPackage {
+            root = lib.sourceFilesBySuffices ./. [ ".cabal" ".hs" ".txt" ];
+            name = name;
+            returnShellEnv = !(devTools == [ ]); # [2]
+
+            modifier = (t.flip t.pipe) [
+              addBuildTools
+              hl.dontHaddock
+              hl.enableStaticLibraries
+              hl.justStaticExecutables
+              hl.disableLibraryProfiling
+              hl.disableExecutableProfiling
+            ];
+          };
+
+      in {
+        packages.pkg = project [ ]; # [3]
+
+        defaultPackage = self.packages.${system}.pkg;
+
+        devShell = project (with haskellPackages; [ # [4]
+          cabal-fmt
+          cabal-install
+          haskell-language-server
+          hlint
+        ]);
+      });
+}
