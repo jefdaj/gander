3782f58b8898ad649cb909eaf1e40cec2db3432d
jefdaj <1198065+jefdaj@users.noreply.github.com>
2023-04-11 21:44:45 -0700

    clean up build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
+
 # Experimental static build script
+
+# If the build fails with 'no space left on device', you may need to enlarge or
+# move TMPDIR. Ideally put it in RAM. Example:
+# sudo mount -o remount,size=30G,nr_inodes=10000000 /run/user/1000
+# export TMPDIR=/run/user/1000
 
 set -x
 
@@ -9,9 +15,10 @@ stack_build() {
 	code=$?
 	[[ $code == 0 ]] || exit $code
 	bindir=$(find .stack-work -type d -name bin | sort | tail -n1)
-	cp "${bindir}/gander" gander-static-${os}
+	cp "${bindir}/gander" gander-${os}
 }
 
+# TODO remove? not getting cache hits anymore
 use_cachix() {
 	nix-env -iA cachix -f https://cachix.org/api/v1/install
 	cachix use static-haskell-nix
@@ -23,13 +30,12 @@ nix_build() {
 	nix build
 	code=$?
 	[[ $code == 0 ]] || exit $code
-	result=$(readlink result)
-	cp ${result}/bin/gander gander-static-${os}
+	cp ./result/bin/gander gander-${os}
 
 }
 
 case $(uname) in
-	Darwin) stack_build macos;;
+	Darwin) stack_build mac;;
 	Linux) nix_build linux;;
 	*) echo "unknown OS! trying nix build"; nix_build unknown;;
 esac
